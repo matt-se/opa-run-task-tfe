dd649ba64a8e8d08702d122d5912ae9d1584a90d
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-02-06 10:30:53 -0500

    Update opa-server.py

--- opa-server.py
+++ opa-server.py
@@ -2,8 +2,6 @@ from flask import Flask, redirect, url_for, request
 from runtask import *
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
-
 
 app = Flask(__name__)
 
@@ -15,15 +13,17 @@ def hello_world():
 @app.route("/runtask", methods = ['GET', 'POST'])
 def run_task():
     app.logger.info(f'incoming {request.method} request on /runtask')
-    app.logger.debug(f'incoming {request.method} request on /runtask')
-    app.logger.warning(f'incoming {request.method} request on /runtask')
     if request.method == 'POST':
         rq = request.json
+        app.logger.info(f'{rq}')
         app.logger.info('Processing - %s on workspace - %s [%s] ', rq['run_id'], rq['workspace_name'], rq['workspace_id'])
         if rq['run_id'] != 'run-xxxxxxxxxxxxxxxx':
             process_opa(app,rq)
         else:
             app.logger.info('Test Run Task Registration message from TFC. Skip processing OPA request')
         return "Ok"
-    else:
+    else:  
       return "GET"
+
+
+
