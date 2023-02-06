577d27ed4f76e1014d202914aee1370089b4bbe9
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-02-06 10:02:16 -0500

    Update opa-server.py

--- opa-server.py
+++ opa-server.py
@@ -11,6 +11,7 @@ def hello_world():
 @app.route("/runtask", methods = ['GET', 'POST'])
 def run_task():
     app.logger.info("incoming request on /runtask")
+    logger.info("incoming request on /runtask")
     print(request.method)
     print(request.json)
     if request.method == 'POST':
